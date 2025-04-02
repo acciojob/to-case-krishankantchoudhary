e09de11995035439a8c6c148e493a3a84f81a48a
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-02 17:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function toCase(text) {
   // write your code here
+	return text.lowertoCase+"-"+text.uppertoCase;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
